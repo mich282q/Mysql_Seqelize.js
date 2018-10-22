9cf5afef07724c7e7f4ef3b3fefb1bd97aede1f2
Michael Trans <mich282q@edu.easj.dk>
2018-10-22 11:19:55 +0200

    github

---
+++ FindUser.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('sequelizejs', 'root', '8305', {
+  host: 'localhost',
+  dialect: 'mysql',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+
+});
+
+const User = sequelize.define('user', {
+  firstName: {
+    type: Sequelize.STRING
+  },
+  lastName: {
+    type: Sequelize.STRING
+  }
+});
+
+  User.findAll().then(users => {
+    console.log(users)
+  })
+
+  
+  
